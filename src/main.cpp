3a53eb0ec10fd40d6f9c17610525e343e09f4211
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2023-12-23 15:16:58 +0400

    feature: 1.2MHz freq

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+
+void setup_clkpr()
+{
+    cli();
+    CLKPR |= (1 << CLKPCE);
+    CLKPR |= ((1 << CLKPS2) && (1 << CLKPS1));
+    sei();
+}
 
 int main()
 {
+    // setup_clkpr();
+
     DDRB |= 1 << DDB0;
 
     for (;;)
     {
-        PORTB |= (1 << PORTB0);
-        _delay_ms(10);
-        PORTB &= ~(1 << PORTB0);
-        _delay_ms(10);
+        PORTB ^= (1 << PORTB0);
+        _delay_ms(1000.0f);
     }
 
     return 0;
