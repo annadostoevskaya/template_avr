f16664068d7ede5f5c67934886e94578affbc964
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2023-12-26 06:14:17 +0600

    feature: _BV

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,13 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-void setup_clkpr()
-{
-    cli();
-    CLKPR |= (1 << CLKPCE);
-    CLKPR |= ((1 << CLKPS2) && (1 << CLKPS1));
-    sei();
-}
-
 int main()
 {
-    // setup_clkpr();
-
-    DDRB |= 1 << DDB0;
-
+    DDRB |= _BV(DDB0);
+    
     for (;;)
     {
-        PORTB ^= (1 << PORTB0);
+        PORTB ^= _BV(PORTB0);
         _delay_ms(1000.0f);
     }
 
